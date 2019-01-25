7a59bed45ac4fbde1069dc87033ed45a7f46b326
wilcockknoxa <ashleywilcock-knox@rentalcars.com>
2019-01-25 12:01:57 +0000

    can now put bears

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    console.log('That tickles!');
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -57,10 +57,54 @@ router.route('/bears')
             if (err)
                 res.send(err);
 
-            res.json({ message: 'Bear created!' });
-        });
+            res.json({ message: bear.name + ' the bear was created!' });
+        })
 
+    })
+
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
     });
+
+// on routes that end iin /bears/:bear_id
+//=============================================================================
+router.route('/bears/:bear_id')
+
+  // get the bear with that id
+  .get(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+      res.json(bear);
+    })
+  })
+
+// update the bear with this id
+.put(function(req, res) {
+
+    // use our bear model to find the bear we want
+    Bear.findById(req.params.bear_id, function(err, bear) {
+
+      if (err)
+        res.send(err);
+
+        bear.name = req.body.name; // update the bears info
+
+        // save the bear
+        bear.save(function(err) {
+          if (err)
+          res.send(err);
+
+          res.json({ message : bear.name + ' was updated'})
+        });
+    });
+});
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
